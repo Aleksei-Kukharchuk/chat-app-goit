544c0f61b918d0f19de65a1caaf51ebc4110447d
Aleksei Kukharchuk <65033359+Aleksei-Kukharchuk@users.noreply.github.com>
2021-05-23 16:40:54 +0300

    Init

---
+++ src/components/message/message.js
@@ -0,0 +1,15 @@
+import "./message.css";
+
+const Message = ({ msg, currentUser }) => {
+  const classDependUser =
+    msg.user === currentUser ? "alert alert-primary" : "alert alert-dark";
+  return (
+    <div className="message-container">
+      <span className={classDependUser}>
+        {msg.user}: {msg.text}
+      </span>
+    </div>
+  );
+};
+
+export default Message;
